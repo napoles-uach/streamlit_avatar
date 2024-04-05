c9201ecf237227c86f2b80519f4deb42b1844327
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-04-05 11:42:10 -0600

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from streamlit_avatar import avatar
 
-text = "Hola, este es un ejemplo."
-lang = "es"
 
 prompt = st.chat_input("Say something",key=0)
-avatar(prompt)#, n_frames, url, lang)
+avatar(prompt)
