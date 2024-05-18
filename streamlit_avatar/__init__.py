65b1856fbf974d2d26063b2c694d17a6814cb874
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-05-18 13:02:26 -0600

    Update __init__.py

--- streamlit_avatar/__init__.py
+++ streamlit_avatar/__init__.py
@@ -12,7 +12,7 @@ def avatar(text='', lang='en-US'):
             100% { background-image: url('https://raw.githubusercontent.com/napoles-uach/streamlit_avatar/main/artic_closed_eyes.png'); }
         """
 
-        keyframes_speaking = "".join([f"{i*10}% {{background-image: url('https://raw.githubusercontent.com/napoles-uach/streamlit_avatar/main/artic_{i}.png');}}\n" for i in range(10)])
+        keyframes_speaking = "".join([f"{i*11}% {{background-image: url('https://raw.githubusercontent.com/napoles-uach/streamlit_avatar/main/artic_{i}.png');}}\n" for i in range(11)])
 
         # Construcción del HTML para el avatar animado.
         html_str = f"""
@@ -78,10 +78,10 @@ def avatar(text='', lang='en-US'):
                     var utterance = new SpeechSynthesisUtterance(texto);
                     utterance.lang = "{lang}"; // Configurar el idioma deseado
                     utterance.onstart = function(event) {{
-                        setAnimation('speakAnimation', 5, 10);
+                        setAnimation('speakAnimation', 2, 10);
                     }};
                     utterance.onend = function(event) {{
-                        setTimeout(() => {{ setAnimation('waitingAnimation', 2, 2); }}, 100);
+                        setTimeout(() => {{ setAnimation('waitingAnimation', 2, 2); }}, 500);
                     }};
                     speechSynthesis.speak(utterance);
 
